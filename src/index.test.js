eb5a13323f059dd2ac47c51a073f0b771eb69edb
ilgor <ilgorbek@gmail.com>
2018-08-16 13:44:06 -0500

    + Added Testing using Mocha

---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import expect from 'expect';
+
+describe('First mock test', () => {
+  it('true is equal to true', () => {
+    expect(true).toEqual(true);
+  });
+});
--- tools/srcServer.js
+++ tools/srcServer.js
@@ -11,7 +11,7 @@ const app = express();
 const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: false,
+  noInfo: true,
   publicPath: config.output.publicPath
 }));
 
